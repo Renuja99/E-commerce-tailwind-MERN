087f930de3530de16f6a9366a9e5ec95838bd593
Renuja <devmindadeco@gmail.com>
2021-07-21 12:20:18 +0530

    Backend routes

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,8 @@ import productRoutes  from './routes/productRoutes.js'
 
 const app = express() //initializing an instance of express
 const port = 3000
-const myLogger = function (req, res, next) {
-    console.log('middleware') 
-    next()  
-}
-const myLogger2 = function (req, res) {
-    console.log('middleware2') 
-    next()
-}
-const myLogger3 = function (req, res) {
-    console.log('middleware3') 
-    res.end();
-}
 
-app.use(myLogger);  //middleware
+
 app.use('/api/products', productRoutes)
 
 app.get('/api/users/create', (req, res,next)=>{  //routeHandlers
@@ -30,8 +18,7 @@ app.get('/api/orders/read', (req, res, next)=>{
     next()
 })
 
-app.use(myLogger2);  //middleware
-app.use(myLogger3);  //middleware
+
 
 app.listen(port,()=>{
     console.log(`app listening at http://localhost:${port} for requests.`)
